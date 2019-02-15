e63fb22f3e520710b282721b223f836fe0794264
Ricardo Mangabeira <rasmangabeira@gmail.com>
2019-02-15 18:14:14 -0300

    Ajustes

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -f dockerfiles/ubuntu/apim/Dockerfile -t jointreinamentosegovernanca/wso2am:2.6.0 dockerfiles/ubuntu/apim/
-docker build -f dockerfiles/ubuntu/apim-analytics/base/Dockerfile -t wso2am-analytics-base:2.6.0 dockerfiles/ubuntu/apim-analytics/base/
-docker build -f dockerfiles/ubuntu/apim-analytics/dashboard/Dockerfile -t wso2am-analytics-dashboard:2.6.0 dockerfiles/ubuntu/apim-analytics/dashboard/
-docker build -f dockerfiles/ubuntu/apim-analytics/worker/Dockerfile -t wso2am-analytics-worker:2.6.0 dockerfiles/ubuntu/apim-analytics/worker/
+docker build -f dockerfiles/ubuntu/apim-analytics/base/Dockerfile -t jointreinamentosegovernanca/wso2am-analytics-base:2.6.0 dockerfiles/ubuntu/apim-analytics/base/
+docker build -f dockerfiles/ubuntu/apim-analytics/dashboard/Dockerfile -t jointreinamentosegovernanca/wso2am-analytics-dashboard:2.6.0 dockerfiles/ubuntu/apim-analytics/dashboard/
+docker build -f dockerfiles/ubuntu/apim-analytics/worker/Dockerfile -t jointreinamentosegovernanca/wso2am-analytics-worker:2.6.0 dockerfiles/ubuntu/apim-analytics/worker/
 docker build -f dockerfiles/ubuntu/is-as-km/Dockerfile -t jointreinamentosegovernanca/wso2is-km:5.7.0 dockerfiles/ubuntu/is-as-km/
---
+++ push-docker-images.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker push jointreinamentosegovernanca/wso2am:2.6.0
+docker push jointreinamentosegovernanca/wso2am-analytics-base:2.6.0
+docker push jointreinamentosegovernanca/wso2am-analytics-dashboard:2.6.0
+docker push jointreinamentosegovernanca/wso2am-analytics-worker:2.6.0
+docker push jointreinamentosegovernanca/wso2is-km:5.7.0
